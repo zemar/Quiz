7198198f0e923e8421c493e755c19442b4516e20
Michael <michael.r808@gmail.com>
2015-02-22 13:15:34 -0600

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by mike on 2/22/15.
+//  Copyright (c) 2015 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
